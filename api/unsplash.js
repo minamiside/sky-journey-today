571d667be3ffdc9b2637ef245e1d68206efafb27
minamiside <nangaoqiao048@gmail.com>
2026-04-13 23:34:14 +0900

    Add files via upload

---
+++ api/unsplash.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const key = process.env.UNSPLASH_KEY;
+  const { query } = req.query;
+
+  const r = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}&per_page=4`,
+    {
+      headers: {
+        Authorization: `Client-ID ${key}`
+      }
+    }
+  );
+
+  const data = await r.json();
+  res.status(200).json(data);
+}
